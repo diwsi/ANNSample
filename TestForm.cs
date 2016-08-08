a266edcc48f5b77e74a861602e16e7084c7f0a8e
diwsi <xenamorphx@gmail.com>
2016-08-09 00:13:12 +0300

    Contact Info Added

--- TestForm.cs
+++ TestForm.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿/// <summary>
+///  Artificial neural network  and backpropagation example
+/// Engin Özdemir 2016
+/// xenamorphx@gmail.com
+/// https://binarysongs.blogspot.com
+/// </summary>
+
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
